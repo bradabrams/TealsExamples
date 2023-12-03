143f2f248f6f14ecffcf8bec86acb6b132ff0533
Brad Abrams <bma@google.com>
2023-12-03 18:22:30 +0000

    inital commit

---
+++ ReviewRunnder.java
@@ -0,0 +1,10 @@
+class ReviewRunner {
+  public static void main(String[] args) 
+  {
+    /* your code here, for example: */
+    System.out.println(Review.sentimentVal("love"));
+    System.out.println(Review.totalSentiment("SimpleReview.txt"));
+    System.out.println(Review.starRating("SimpleReview.txt"));
+
+  }
+}
